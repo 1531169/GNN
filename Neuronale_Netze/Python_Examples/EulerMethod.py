7862c93f570a1fac6eb6d4bd92cef892eb33c495
Cedric einhard <1531169@stud.hs-mannheim.de>
2018-03-31 00:54:05 +0200

    created some example function and displayed

--- Neuronale_Netze/Python_Examples/EulerMethod.py
+++ Neuronale_Netze/Python_Examples/EulerMethod.py
@@ -5,29 +5,61 @@ Created on 30.03.2018
 '''
 from array import array
 import matplotlib.pyplot as plt
+import numpy as np
 
 class EulerMethod(object):
-    '''
-    classdocs
-    '''
+    config:dict = {
+        'yTicks': 1,
+        'yMin'  : -10,
+        'yMax'  : 5,
+        'yLabel': 'x (Wert)',
 
+        'xTicks': 0.1,
+        'xMin'  : -5,
+        'xMax'  : 10,
+        'xLabel': 't (Zeit)'
+    }
 
-    def __init__(self, deltaT):
-        self.deltaT = deltaT
+    '''
+    a function the will be displayed and takes only float param!
+    '''
+    func = None
+
+    def __init__(self, function):
+        self.func = function
+        plt.yticks = self.config.get('yTicks')
+        plt.xlim(self.config.get('yMin'), self.config.get('yMax'))
+        plt.xticks = self.config.get('xTicks')
+        plt.xlim(self.config.get('xMin'), self.config.get('xMax'))
+        plt.ylabel(self.config.get('yLabel'))
+        plt.xlabel(self.config.get('xLabel'))
     
     def explicitMethod(self, initialConditions:array, deltaT):
         return 0
     
     def getNextStep(self, curStep = 0):
-        return curStep + self.deltaT
+        return curStep + self.config.get('xTicks')
     
-    def getRealFunctionByRange(self, xFrom, xTo):
-        return 0
+    def getRealFunctionByRange(self, xFrom:float, xTo:float):
+        y:np.array = []
+        x:np.array = []
+
+        for i in np.arange(xFrom, xTo, self.config.get('xTicks')):
+            stepResult = self.func(i)
+            # checking ranges to optimize view (bc. 0.1 steps makes much data)
+            inYAxisRange = self.config.get('yMin') <= stepResult <= self.config.get('yMax')
+            inXAxisRange = self.config.get('xMin') <= i <= self.config.get('xMax')
+
+            if inYAxisRange and inXAxisRange:
+                x.append(i)
+                y.append(stepResult)
+        
+        return np.array([x, y])
+
+    def addFunctionData(self, data:np.array):
+        plt.plot(data[0], data[1])
     
     def showPlot(self):
-        plt.plot([0.1, 0.2, 0.3], [-7, -0.2, 8])
-        plt.ylabel('x')
-        plt.xlabel('t')
         plt.show()
 
 
--- Neuronale_Netze/__init__.py
+++ Neuronale_Netze/__init__.py
@@ -1,7 +1,20 @@
 from Python_Examples import example_1 as Examples
 from Python_Examples import example_2 as Examples
 from Python_Examples.EulerMethod import EulerMethod
+import numpy as np
 
-eulerCalc = EulerMethod(0.1)
+# the function to calculate
+def y(x:float):
+    return (x - (x * x * x))
+
+# gets a function that will be used
+eulerCalc = EulerMethod(y)
+
+# get range of calculation
+result = eulerCalc.getRealFunctionByRange(-10.0, 10.0)
+eulerCalc.addFunctionData(result)
+# multiply with -1 to get different example
+result = [result[0] * -1 + 1.25, result[1]]
+eulerCalc.addFunctionData(result)
 
 eulerCalc.showPlot()
